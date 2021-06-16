0491ef9a1ceec915f0b9d5bc5520216e2ca37e9c
JunhwanKang <wnsghks1017@naver.com>
2021-06-17 00:03:43 +0900

    오늘의 공부 (@initBinder, 객체, 리스트, 배열 파라미터로 받기) [코드로 배우는 스프링 웹 프로젝트]

--- ex01/src/main/java/com/cdj/controller/SampleController.java
+++ ex01/src/main/java/com/cdj/controller/SampleController.java
@@ -1,9 +1,21 @@
 package com.cdj.controller;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.cdj.domain.SampleDTO;
+import com.cdj.domain.SampleDTOList;
+import com.cdj.domain.TodoDTO;
 
 import lombok.extern.log4j.Log4j;
 
@@ -11,6 +23,12 @@ import lombok.extern.log4j.Log4j;
 @RequestMapping("/sample/*")
 @Log4j
 public class SampleController {
+	
+//	@InitBinder
+//	public void initBinder(WebDataBinder binder) {
+//		SimpleDateFormat dataFormat = new SimpleDateFormat("yyyy-MM-dd");
+//		binder.registerCustomEditor(java.util.Date.class, new CustomDateEditor(dataFormat, false));
+//	}
 	
 	@RequestMapping("")
 	public void basic() {
@@ -22,8 +40,52 @@ public class SampleController {
 		log.info("basic get.....");
 	}
 	
+	// 간편하지만 제한적 (스프링 버전 4.3부터...)
 	@GetMapping("/basicOnlyGet")
 	public void basicGet2() {
 		log.info("basic get only get....");
+	}
+	
+	@RequestMapping(value = "/ex01", method = RequestMethod.GET)
+	public String ex01(SampleDTO dto) {
+		log.info("===="+dto);
+		
+		return "ex01";
+	}
+	
+	@RequestMapping(value = "/ex02")
+	public String ex02(@RequestParam("name") String name, @RequestParam("age") int age) {
+		log.info("name:" + name);
+		log.info("age: " + age);
+		
+		return "ex02";
+	}
+	
+	@RequestMapping(value = "/ex02List")
+	public String ex02List(@RequestParam("ids")ArrayList<String> ids) {
+		log.info("ids: " + ids);
+		
+		return "ex02List";
+	}
+	
+	@RequestMapping(value = "/ex02Array")
+	public String ex02Array(@RequestParam("ids") String[] ids) {
+		log.info("array ids: " + Arrays.toString(ids));
+		
+		return "ex02Array";
+	}
+	
+	@RequestMapping(value = "/ex02Bean")
+	public String ex02Bean(SampleDTOList list) {
+		log.info("list dtos: " + list);
+		
+		return "ex02Bean";
+	}
+	
+	@RequestMapping(value = "/ex03")
+	public String ex03(TodoDTO todo) {
+		log.info("todo: " + todo);
+		
+		return "ex03";
 	}
 }
---
+++ ex01/src/main/java/com/cdj/domain/SampleDTO.java
@@ -0,0 +1,10 @@
+package com.cdj.domain;
+
+import lombok.Data;
+
+// setter, getter, equals(), toString() 메서드를 자동 생성해줌
+@Data
+public class SampleDTO {
+	private String name;
+	private int age;
+}
