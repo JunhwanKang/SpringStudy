861ed1a89f9936c905bb346acedc9afb40eba42c
JunhwanKang <wnsghks1017@naver.com>
2021-08-09 22:59:51 +0900

    오라클 연결 mapper 사용해서 조회 테스트 코드 작성

--- ex02/src/test/java/org/zerock/persistence/DataSourceTests.java
+++ ex02/src/test/java/org/zerock/persistence/DataSourceTests.java
@@ -1,5 +1,30 @@
 package org.zerock.persistence;
 
-public class DataSourceTests {
+import static org.junit.Assert.fail;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import org.junit.Test;
+
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+public class DataSourceTests {
+	static {
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	@Test
+	public void testConnection() {
+		try(Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE", 
+				"spring", "1234")){
+			log.info(con);;
+		} catch(Exception e) {
+			fail(e.getMessage());
+		}
+	}
 }
